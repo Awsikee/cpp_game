fffafc4283fc983406cad846d8d604fddde6e994
Krzysztof <65894695+Awsikee@users.noreply.github.com>
2023-12-15 16:41:41 +0100

    sdl win impl

--- inc/game/game.hpp
+++ inc/game/game.hpp
@@ -2,8 +2,31 @@
 #ifndef GAMECLASS_HPP
 #define GAMECLASS_HPP
 
+#include "../inc/include/SDL2/SDL.h"
+#include <iostream>
 class Game
 {
+    private:
+        bool isRunning;
+        SDL_Window *window;
+        SDL_Renderer *renderer;
+        int count;
+    
+    public:
+        Game();
+        ~Game();
+
+        void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+        void handleEvents();
+        void update();
+        void render();
+        void clean();
+
+        inline bool running(){return isRunning;}
+
+
+
 
 };
 
